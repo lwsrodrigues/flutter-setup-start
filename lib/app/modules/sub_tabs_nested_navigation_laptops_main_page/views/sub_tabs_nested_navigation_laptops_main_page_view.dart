1d20b81bb12ed1581a5c2f15bb905b080317a323
sajjad Alizadeh <samycrip10@gmail.com>
2023-12-10 10:09:42 +0330

    nested route

--- lib/app/modules/sub_tabs_nested_navigation_laptops_main_page/views/sub_tabs_nested_navigation_laptops_main_page_view.dart
+++ lib/app/modules/sub_tabs_nested_navigation_laptops_main_page/views/sub_tabs_nested_navigation_laptops_main_page_view.dart
@@ -5,18 +5,17 @@ import 'package:flutter_starter/constants.dart';
 import '../../../../constants.dart';
 import '../controllers/sub_tabs_nested_navigation_laptops_main_page_controller.dart';
 
-class SubTabsNestedNavigationLaptopsMainPageView
-    extends GetView<SubTabsNestedNavigationLaptopsMainPageController> {
+class SubTabsNestedNavigationLaptopsMainPageView extends GetView<SubTabsNestedNavigationLaptopsMainPageController> {
+  const SubTabsNestedNavigationLaptopsMainPageView({super.key});
+
   @override
   Widget build(BuildContext context) {
     return controller.obx(
       (data) => ListView.builder(
         itemBuilder: (BuildContext context, int index) => ListTile(
-          title: Text(data![index]),
-          onTap: () => Get.toNamed(
-              Routes.SUB_TABS_NESTED_NAVIGATION_LAPTOP_DETAIL_PAGE,
-              arguments: data[index],
-              id: Constants.subTabLaptopNavigatorId),
+          title: Text(data[index]),
+          onTap: () => Get.toNamed(Routes.SUB_TABS_NESTED_NAVIGATION_LAPTOP_DETAIL_PAGE,
+              arguments: data[index], id: Constants.subTabLaptopNavigatorId),
         ),
         itemCount: data!.length,
       ),
